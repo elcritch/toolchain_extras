3760ad6e00040c109da0488093d0205c8703f661
Jaremy Creechley <creechley@gmail.com>
2018-05-11 16:04:44 -0600

    Cleaning up package some

--- mix.exs
+++ mix.exs
@@ -1,1 +1,48 @@
+defmodule ToolchainExtras.MixProject do
+  use Mix.Project
+
+  @app :toolchain_extras
+
+  def project do
+    [
+      app: @app,
+      version: "0.2.0",
+      elixir: "~> 1.4",
+      nerves_package: [type: :toolchain_extras],
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      deps: deps()
+    ]
   end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Jaremy Creechley"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/elcritch/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "README.md",
+      "LICENSE"
+      "mix.exs",
+      "lib",
+      "config",
+    ]
+  end
+
+  defp deps do
+    [
+      {:nerves, "~> 1.0", runtime: false},
+    ]
+  end
+end
