3e842f7018fdd0f7ca400269d222a8e3eba9a4bc
Jaremy Creechley <creechley@gmail.com>
2018-08-08 13:36:46 -0600

    Fixing weird regression with paths

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ToolchainExtras.MixProject do
   def project do
     [
       app: @app,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.4",
       nerves_package: [type: :toolchain_extras],
       start_permanent: Mix.env() == :prod,
